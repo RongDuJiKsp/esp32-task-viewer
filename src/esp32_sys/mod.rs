55153fb7c92893aa5fa604ca407a7dc4a98c72c8
yuqizhou <rdjksp@gmail.com>
2026-06-18 14:40:09 +0800

    style: fmt

--- src/esp32_sys/mod.rs
+++ src/esp32_sys/mod.rs
@@ -1,4 +1,4 @@
-pub mod sys_init;
-pub mod panic_handler;
 pub mod display_raw;
+pub mod panic_handler;
 pub mod slint_st7305_platform;
+pub mod sys_init;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use esp32_sys::sys_init::SysInit;
 slint::include_modules!();
 fn main() {
     SysInit::init_sys();
-    
+
     log::info!("Booting ESP32 Task Viewer...");
     let ui = App::new().unwrap();
     ui.run().unwrap();
